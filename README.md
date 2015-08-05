5e08b4c83902c7a7eca74edef6c47de329303686
Frank Förster <foersterfrank@gmx.de>
2015-08-06 01:53:08 +0200

    Inital creation of the module structure using h2xs

--- README.md
+++
@@ -1,2 +0,0 @@
-# Run-Utax
-A wrapper script that runs utax for a given input set
---
+++ lib/Run/Utax.pm
@@ -0,0 +1,60 @@
+package Run::Utax;
+
+use 5.010000;
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.1';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Run::Utax - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Run::Utax;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Run::Utax, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Frank Förster, E<lt>frank@E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by Frank Förster
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
