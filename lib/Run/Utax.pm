c08455bf86171f8d5d8580a891fdb098799d0e98
Frank Förster <foersterfrank@gmx.de>
2015-08-06 19:10:50 +0200

    Changed documentation of Run::Utax

--- lib/Run/Utax.pm
+++ lib/Run/Utax.pm
@@ -13,37 +13,18 @@ our $VERSION = '0.1';
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Run::Utax - Perl extension for blah blah blah
+Run::Utax - Perl module which is utilized by the run_utax.pl script
 
 =head1 SYNOPSIS
 
   use Run::Utax;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for Run::Utax, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-
 =head1 SEE ALSO
-
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
