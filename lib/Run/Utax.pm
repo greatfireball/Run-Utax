2b2358fefb92e08eb1f096d0d29ea6ffae63475e
Frank Förster <foersterfrank@gmx.de>
2015-08-06 11:24:35 +0200

    Corrected email address and licence information

--- lib/Run/Utax.pm
+++ lib/Run/Utax.pm
@@ -35,6 +35,7 @@ Blah blah blah.
 
 =head1 SEE ALSO
 
+
 Mention other useful documentation such as the documentation of
 related modules or operating system documentation (such as man pages
 in UNIX), or any relevant external documentation such as RFCs or
@@ -46,15 +47,30 @@ If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-Frank Förster, E<lt>frank@E<gt>
+Frank Förster, E<lt>foersterfrank@gmx.de<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2015 by Frank Förster
+The MIT License (MIT)
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.2 or,
-at your option, any later version of Perl 5 you may have available.
+Copyright (c) 2009-2015 Frank Förster
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 =cut
